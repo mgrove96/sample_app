7e23c034a88e6ae3026a645717c72b859199d97d
Mitch Grove <mgrove96@gmail.com>
2016-03-08 16:31:25 -0800

    Routed to home page

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,15 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
+  root 'static_pages#home'
 
-  # Example of regular route:
+  # Example of regular route:key => "value", 
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route (maps HTTP verbs to controller applications automatically):
   #   resources :products
 
   # Example resource route with options:
